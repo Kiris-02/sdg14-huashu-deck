442886a0c74b4a74a89f3ff4af5d66113746e86d
Kiris-02 <skrongdattv1@gmail.com>
2026-09-07 00:31:06 +0700

    feat: initial release of SDG 14.1-14.3 Huashu Cinematic Presentation Deck

---
+++ start.bat
@@ -0,0 +1,9 @@
+﻿@echo off
+title SDG 14.1-14.3 Presentation Deck - Huashu Engine
+echo ===================================================================
+echo     SDG 14.1, 14.2 & 14.3 - Huashu Cinematic Presentation Deck
+echo ===================================================================
+echo.
+echo Launching presentation in your default browser...
+start "" "%~dp0index.html"
+exit
